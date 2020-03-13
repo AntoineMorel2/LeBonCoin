f90e28e36c60d0f7367802592e07a67809a907fb
Vincent Pingrenon <vincent.pingrenon@gmail.com>
2020-03-13 09:55:28 +0100

    Project setup

---
+++ src/hibernate/UserEntity.java
@@ -0,0 +1,36 @@
+package hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user", schema = "leboncoin", catalog = "")
+public class UserEntity {
+    private int iduser;
+
+    @Id
+    @Column(name = "iduser", nullable = false)
+    public int getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(int iduser) {
+        this.iduser = iduser;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return iduser == that.iduser;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(iduser);
+    }
+}
