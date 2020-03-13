788996e861ea695ae4c2095e95d2b863c301f239
antoine <antoinemorel78@gmail.com>
2020-03-13 13:42:15 +0100

    push Category brouillon

---
+++ src/hibernate/Category.java
@@ -0,0 +1,33 @@
+package hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    private int idCategory;
+    private String name;
+
+    @Id
+    @Column(name = "idCategory", nullable = false)
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
