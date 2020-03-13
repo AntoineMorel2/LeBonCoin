79e345c9278b73482145722d5253ecd8ab87453d
antoine <antoinemorel78@gmail.com>
2020-03-13 14:46:32 +0100

    Refacto Comment en CommentEntity

--- src/hibernate/CommentEntity.java
+++ src/hibernate/CommentEntity.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "comment")
-public class Comment {
+public class CommentEntity {
     @Id
     @Column(name = "idcomment")
     private int idComment;
