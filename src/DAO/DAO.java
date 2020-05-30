cd4b71ac13d5c527e5fb137cc4204222c9dfab5c
Vincent Pingrenon <vincent.pingrenon@gmail.com>
2020-05-30 18:04:11 +0200

    Added DAO Method and changed name for 2 method

--- src/DAO/DAO.java
+++ src/DAO/DAO.java
@@ -1,13 +1,35 @@
 package DAO;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.util.Collection;
 
-public interface DAO<T> {
-    boolean create(T obj);
-    T fetch(int id);
-    Collection<T> fetchAll();
-    Collection<T> fetchByName();
-    boolean update(T obj);
-    boolean delete(T obj);
-    void close(); // Connection closure of the data source
+public abstract class DAO<T> {
+    public abstract boolean create(T obj);
+    public abstract T fetch(int id);
+    public abstract Collection<T> fetchAll();
+    public abstract Collection<T> fetchByName();
+    public abstract boolean update(T obj);
+    public abstract boolean delete(T obj);
+    public abstract void close(); // Connection closure of the data source
+
+    protected static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    protected static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
 }
+
