32a17174889760b5cef4b440293dc91ffbeb11a0
Vincent Pingrenon <vincent.pingrenon@gmail.com>
2020-06-05 22:48:08 +0200

    SignUp is now working properly

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,15 @@
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import view.CreateAnnonce;
 import view.SignInView;
 
 import javax.swing.*;
 
 public class Main {
-    private static final SessionFactory ourSessionFactory;
     private static SignInView signInView;
-    private static CreateAnnonce createannonce;
 
-    static {
-        try {
-            Configuration configuration = new Configuration();
-            configuration.configure();
-
-            ourSessionFactory = configuration.buildSessionFactory();
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static Session getSession() throws HibernateException {
-        return ourSessionFactory.openSession();
-    }
-
-    public static void main(final String[] args) throws Exception {
-        final Session session = getSession();
-        try {
+    public static void main(final String[] args) {
             signInView = new SignInView();
-            System.out.println("querying all the managed entities...");
-           //accueilView = new AccueilView();
-            //createannonce = new CreateAnnonce();
-            //JFrame frame = new JFrame("AccueilView");
             signInView.setContentPane(signInView.accueil);
             signInView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             signInView.pack();
             signInView.setVisible(true);
-//            accueilView.setContentPane(accueilView.accueil);
-//            accueilView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            accueilView.pack();
-//            accueilView.setVisible(true);
-
-           /* createannonce.setContentPane(createannonce.createannonce);
-            createannonce.pack();
-            createannonce.setVisible(true);*/
-        } finally {
-            session.close();
-        }
     }
 }
--- src/view/SignUpView.java
+++ src/view/SignUpView.java
@@ -1,5 +1,9 @@
 package view;
 
+import DAO.UserDAO;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import hibernate.UserEntity;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,8 +16,8 @@ public class SignUpView extends JFrame{
     private JTextField textField1;
     private JTextField textField2;
     private JButton inscriptionButton;
-
-    private AnnonceItemView annonceItemView = new AnnonceItemView();
+    private JTextField nomUtilisateur;
+    private JTextField prenomUtilisateur;
 
     public SignUpView() {
         add(signUpPanel);
@@ -21,6 +25,30 @@ public class SignUpView extends JFrame{
         setTitle("Inscription");
         setPreferredSize(new Dimension(500, 500));
 
-        //signUpPanel.add(annonceView, new com.intellij.uiDesigner.core.GridConstraints());
+        inscriptionButton.addActionListener(actionEvent -> {
+            String password1 = Base64.encode((new String(passwordField1.getPassword())).getBytes());
+            String password2 = Base64.encode((new String(passwordField2.getPassword())).getBytes());
+            if (!password1.equals(password2)) {
+                JOptionPane.showMessageDialog(signUpPanel, "Vos mots de passe ne correspondent pas, veuillez changer.");
+            } else {
+                if (!textField1.getText().equals(textField2.getText())) {
+                    JOptionPane.showMessageDialog(signUpPanel, "Vos emails ne correspondent pas, veuillez changer.");
+                } else {
+                    try {
+                        UserEntity newUser = new UserEntity(nomUtilisateur.getText(), prenomUtilisateur.getText(), textField1.getText(), password1);
+                        UserDAO uDao = new UserDAO();
+                        if (uDao.create(newUser)) {
+                            JOptionPane.showMessageDialog(signUpPanel, "L'utilisateur avec le prénom " + prenomUtilisateur.getText() + " a bien été ajouté");
+                            setVisible(false);
+                        } else {
+                            JOptionPane.showMessageDialog(signUpPanel, "Impossible d'ajouter l'utilisateur, veuillez réessayer");
+                        }
+                    } catch (IllegalArgumentException e) {
+                        JOptionPane.showMessageDialog(signUpPanel, "Une erreur est apparue lors de l'ajout de l'utilisateur vérifier le bon format de l'email");
+                    }
+
+                }
+            }
+        });
     }
 }
