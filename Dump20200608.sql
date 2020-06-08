1b77758e47d5ae75af38be56600f024e58e127a6
Anaïs Roussel <anais.rousl@gmail.com>
2020-06-08 09:49:47 +0200

    Dump DB

--- Dump20200608.sql
+++ Dump20200608.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `leboncoin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `leboncoin`;
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: leboncoin
 -- ------------------------------------------------------
@@ -39,7 +37,7 @@ CREATE TABLE `annonce` (
   KEY `idUser_idx` (`iduser`),
   CONSTRAINT `idcategoryannonce` FOREIGN KEY (`idcategory`) REFERENCES `category` (`idcategory`),
   CONSTRAINT `idUser` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +46,7 @@ CREATE TABLE `annonce` (
 
 LOCK TABLES `annonce` WRITE;
 /*!40000 ALTER TABLE `annonce` DISABLE KEYS */;
-INSERT INTO `annonce` VALUES (1,'Raoult cherche la merde','Le docteur Raoult cherche quelque chose qu\'il peut enfin trouver correctement: la merded',0,'2020-06-03 22:00:00',12,1,1);
+INSERT INTO `annonce` VALUES (22,'Ma première annonce le coin bon','Voici une première annonce pour tester notre application LeCoinBon',0,'2020-06-06 22:00:00',10,1,8),(23,'Ma seconde annonce leCoinBon','Lorem ipsum dolor sit amet, consectetur adipiscing elit,  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in  reprehenderit in voluptate velit esse cillum dolore  eu fugiat nulla pariatur. Excepteur sint occaecat',0,'2020-06-06 22:00:00',11,2,8),(24,'Titre','\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"jhkjhklhhjhjhkhkjhjkhjh',0,'2020-06-06 22:00:00',10,4,8),(25,'lorem ipsum','Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',0,'2020-06-06 22:00:00',20,5,8),(26,'Nouvelle annonce ','lorem ipsum ...',0,'2020-06-06 22:00:00',10,3,8);
 /*!40000 ALTER TABLE `annonce` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,7 +71,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'BBC'),(2,'TF1'),(3,'CNN'),(4,'AL Jazeera'),(5,'BFMTV');
+INSERT INTO `category` VALUES (1,'Meuble'),(2,'Jardin'),(3,'Informatique'),(4,'Mobile'),(5,'Services');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +94,7 @@ CREATE TABLE `comment` (
   KEY `idannoncecomment_idx` (`idannonce`),
   CONSTRAINT `idannoncecomment` FOREIGN KEY (`idannonce`) REFERENCES `annonce` (`idannonce`),
   CONSTRAINT `idusercomment` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +103,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,1,'2020-06-04','Haha, bien fait pour sa gueule, il le mérite ce chercheur de pacotille.',2),(2,1,'2020-06-04','Ouais, j\'avoue, il a abusé le mec!',3);
+INSERT INTO `comment` VALUES (24,24,'2020-06-06','Un premier commentaire',4),(25,24,'2020-06-06','deuxième commentaire\n',8);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,7 +123,7 @@ CREATE TABLE `image` (
   UNIQUE KEY `idimage_UNIQUE` (`idimage`),
   KEY `idannonce_idx` (`idAnnonce`),
   CONSTRAINT `idannonce` FOREIGN KEY (`idAnnonce`) REFERENCES `annonce` (`idannonce`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,7 +132,7 @@ CREATE TABLE `image` (
 
 LOCK TABLES `image` WRITE;
 /*!40000 ALTER TABLE `image` DISABLE KEYS */;
-INSERT INTO `image` VALUES (1,1,'../../pictures/Raoult.jpg','Raoult');
+INSERT INTO `image` VALUES (22,22,'src/ressources/32453907-4b99-48a0-ba2f-6ed372ca33f3.jpg','32453907-4b99-48a0-ba2f-6ed372ca33f3'),(23,23,'src/ressources/622781cd-2f4f-4995-a05b-108f0fa037b3.jpg','622781cd-2f4f-4995-a05b-108f0fa037b3'),(24,24,'src/ressources/9ce3e7ad-a2b8-4035-bbb8-9d15e674126f.jpg','9ce3e7ad-a2b8-4035-bbb8-9d15e674126f'),(25,25,'src/ressources/7b824a90-7511-4a3a-bf85-f3603486382c.jpg','7b824a90-7511-4a3a-bf85-f3603486382c'),(26,26,'src/ressources/923659fa-4881-450a-b5b9-fc90985f5778.jpg','923659fa-4881-450a-b5b9-fc90985f5778');
 /*!40000 ALTER TABLE `image` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,7 +151,7 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`iduser`),
   UNIQUE KEY `iduser_UNIQUE` (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,7 +160,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Vert chat voeux','Valentin','valentin.jmenbalek@gmail.com','123'),(2,'Mot Relle','Sur?','antoinemorel78@gmail.com','456'),(3,'Roux selle','Pranko','jaiPeurDesFantomes@gmail.com','789'),(4,'Pain Grr euh non','Vingt centimes','onMangeQuand@gmail.com','password');
+INSERT INTO `user` VALUES (1,'Vert chat voeux','Valentin','valentin.j@gmail.com','123'),(2,'Mot Relle','Sur?','antoinemorel78@gmail.com','456'),(3,'Roux selle','Pranko','jaiPeurDesFantomes@gmail.com','789'),(4,'Pain Grr euh non','Vingt centimes','onMangeQuand@gmail.com','password'),(8,'Roussel','Anaïs','anais.rousl@gmail.com','MTIz'),(9,'Vallaeys','Harry','harry@gmail.com','MTIz');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -175,4 +173,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-05 19:47:30
+-- Dump completed on 2020-06-08  9:45:19
